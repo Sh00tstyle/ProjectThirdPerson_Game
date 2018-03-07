687c0937cde19cb348641b662f72e8e4e24c0e87
Daan <daankemps@hotmail.com>
2018-03-07 15:47:15 +0100

    Added new font and added star ratings to levels (currently just static numbers)

--- assets/mge/lua/ui.lua
+++ assets/mge/lua/ui.lua
@@ -20,6 +20,7 @@ Menus = {
 -- Name = 1, Path == 2
 Fonts = {
   {"Arial", "arial.ttf"},
+  {"Shark", "sharkformalfunnyness.ttf"},
 }
 
 -- Target Levels should be LEVEL 1, LEVEL 2, etc.
@@ -31,7 +32,7 @@ Buttons = {
   { "MAIN",             "EXIT",             "mainmenu/quit.png",          "mainmenu/quit_deactivated.png",              145,    675 },  
   { "Controls",         "MAIN",             "backbutton.png",             "backbutton_deactivated.png",                  1700,   830 },  
   { "Credits",          "MAIN",             "backbutton.png",             "backbutton_deactivated.png",                 1700,   830 },
-  { "LevelSelect 1",    "LevelSelect 0",    "nextbutton.png",             "nextbutton_deactivated.png",                 125,    412 },
+  { "LevelSelect 1",    "LevelSelect 0",    "previousbutton.png",         "previousbutton_deactivated.png",                 82,    490 },
   { "PAUSE",            "RESUME",           "pausemenu/continue.png",     "pausemenu/continue_deactivated.png",         800,    400 },
   { "PAUSE",            "RESET",            "pausemenu/reset.png",        "pausemenu/reset_deactivated.png",         800,    600 },
   { "PAUSE",            "MAIN",             "pausemenu/menu.png",         "pausemenu/menu_deactivated.png",         800,    800 },
@@ -41,16 +42,31 @@ Buttons = {
 Images = { 
   {"HUD",             "hud/tries_panel.png",                     40,       980 }, 
   {"Controls",        "controls/controls.png",            105,      250 },
-  {"Credits",         "credits/credits.png",              105,      250 },
+  {"Credits",         "levelselection/panel.png",              105,      250 },
   {"PAUSE",           "pausemenu/panel.png",             730,      250 }, 
   {"LevelSelect 0",   "levelselection/panel.png",         105,      250 },
   {"LevelSelect 1",   "levelselection/panel.png",         105,      250 },
 }
 
+
+LevelRating = { 0, 1, 2, 1, 2, 1, 1, 1, 0, 0 }
+
+
 -- Target Menu = 1 Font Name used = 2, Drawn Text = 4, text size = 5, textcolor r = 6, textcolor g = 7, textcolor b = 8, boldness = 9, posX = 10, posY = 11
 Text = {
-  { "LevelSelect 0", "Arial", "Level 1", 25, 0, 0, 0, true, 150, 700},
   { "HUD", "Arial", "Tries: TRIES", 25, 0, 0, 0, true, 80, 1000},
+  {"Credits", "Shark", "Daan Kemps", 75, 10, 10 , 10, true, 230, 320},
+  {"Credits", "Shark", "Felix de Natris", 75, 10, 10 , 10, true, 230, 400},
+  {"Credits", "Shark", "Lucia Dzediti", 75, 10, 10 , 10, true, 230, 480},
+  {"Credits", "Shark", "Nico Mazek ", 75, 10, 10 , 10, true, 230, 560},
+  {"Credits", "Shark", "Anna Richter", 75, 10, 10 , 10, true, 230, 640},
+  {"Credits", "Shark", "Alex Lysenko", 75, 10, 10 , 10, true, 230, 720},
+  {"Credits", "Shark", "ROLE", 75, 10, 10 , 10, true, 1050, 320},
+  {"Credits", "Shark", "ROLE", 75, 10, 10 , 10, true, 1050, 400},
+  {"Credits", "Shark", "ROLE", 75, 10, 10 , 10, true, 1050, 480},
+  {"Credits", "Shark", "ROLE", 75, 10, 10 , 10, true, 1050, 560},
+  {"Credits", "Shark", "ROLE", 75, 10, 10 , 10, true, 1050, 640},
+  {"Credits", "Shark", "ROLE", 75, 10, 10 , 10, true, 1050, 720},
 }
 
 
@@ -72,29 +88,33 @@ function addLevelResolutionScreens()
     newBackButton = {"LEVEL " .. i, "MAIN", "backbutton.png", "backbutton_deactivated.png", 1200,  830 },
     table.insert(Buttons, newBackButton)
     
-    newText =  { "LEVEL " .. i, "Arial", "You completed Level " .. i .. " in " .. "TRIES" .. " tries", 45, 0, 0, 0, true, 650, 540},
+    newText =  { "LEVEL " .. i, "Shark", "You completed Level " .. i .. " in " .. "TRIES" .. " tries", 50, 0, 0, 0, true, 660, 540},
     table.insert(Text, newText)
     
     -- Insert star rating
     for a=0, 2 do
-      newStar = { "LEVEL " .. i,   "resolutionscreen/star.png", starX + offset * a, starY }
-      table.insert(Images, newStar)
+     if(a <= LevelRating[i]) then
+        activatedStar = { "LEVEL " .. i,   "resolutionscreen/star.png", starX + offset * a, starY }
+        table.insert(Images, activatedStar)
+      elseif (a > LevelRating[i]) then
+        deactivatedStar = { "LEVEL " .. i,   "resolutionscreen/star_deactivated.png", starX + offset * a, starY }
+        table.insert(Images, deactivatedStar)
+      end
     end
   end
  end
 
 function addLevelSelection()
--- Add Menus
-
 LevelSelect = 0
 
-thumbnailX = 160 
-thumbnailY = 366
-backgroundX = 150 
-backgroundY = 355
+thumbnailX = 165 
+thumbnailY = 360
+backgroundX = 155 
+backgroundY = 350
 
 imageOffset = 325
 offsetmodifier = 0
+staroffset = 50
 levelSelectionBackground = "levelselection/level_bg.png"
 
   for i=1, levelcount do
@@ -107,13 +127,30 @@ levelSelectionBackground = "levelselection/level_bg.png"
     thumbnailBackground = { "LevelSelect " .. LevelSelect, levelSelectionBackground, backgroundX + imageOffset * offsetmodifier, backgroundY} 
     table.insert(Images, thumbnailBackground)
     
-    
     --Level Text
-    --newText = {"LevelSelect 0", "Arial", "Level " .. i, 12, 0, 0, 0, true, backgroundX + imageOffset * offsetmodifier, backgroundY + 125 }
-    --table.insert(Text, newText)
-    --print(newText)
+    levelText = {"LevelSelect " .. LevelSelect, "Shark", "Level " .. i, 52, 0, 0, 0, true, 86 + backgroundX + imageOffset * offsetmodifier, backgroundY + 380 }
+    table.insert(Text, levelText)
+    
+    -- Tries text
+    levelText = {"LevelSelect " .. LevelSelect, "Arial", "TRIES", 18, 0, 0, 0, false, 20 + backgroundX + imageOffset * offsetmodifier, backgroundY + 320 }
+    table.insert(Text, levelText)
+    
+    
+    -- Insert star activated and deactivated stars;
+    for a=0, 2 do
+      if(a <= LevelRating[i]) then
+        activatedStar = { "LevelSelect " .. LevelSelect, "levelselection/star_small.png", (thumbnailX + 115 + 50 * a) 
+          + imageOffset * offsetmodifier, thumbnailY + 300}
+        table.insert(Images, activatedStar)
+      elseif (a > LevelRating[i]) then
+        deactivatedStar = { "LevelSelect " .. LevelSelect, "levelselection/star_small_deactivated.png", (thumbnailX + 115 + 50 * a) 
+          + imageOffset * offsetmodifier, thumbnailY + 300}
+        table.insert(Images, deactivatedStar)
+      end
+    end
     
     offsetmodifier  = offsetmodifier + 1
+    
     -- Reset variables once 5 levels have been displayed
     if(i == 5) then
       LevelSelect = 1
@@ -122,12 +159,11 @@ levelSelectionBackground = "levelselection/level_bg.png"
   end
   
   -- Additional Buttons
-  table.insert(Buttons,{ "LevelSelect 0", "LevelSelect 1","nextbutton.png", "nextbutton_deactivated.png",  1754, 412 })
+  table.insert(Buttons,{ "LevelSelect 0", "LevelSelect 1","nextbutton.png", "nextbutton_deactivated.png",  1732, 490 })
   table.insert(Buttons,{ "LevelSelect 1", "MAIN", "backbutton.png","backbutton_deactivated.png",      1700, 830 })
   table.insert(Buttons,{ "LevelSelect 0", "MAIN", "backbutton.png","backbutton_deactivated.png",      1700, 830 }) 
-  
-  
- end
+ 
+end
 
 -- returns the amount of images or buttons for each menu/parent
 function count(class, string)
@@ -139,6 +175,7 @@ local count = 0;
   end
   return count;
 end
+
 
 function Menu.init()
 -- Add all thumbnails, images and buttons for the levelselection screens to the array
